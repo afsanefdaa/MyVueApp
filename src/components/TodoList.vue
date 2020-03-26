cffe7bfbe9493a9b4eedc40e5e692953842e80b0
Afsane <afsanefadaei@hotmail.com>
2020-03-26 22:11:46 +0430

    transition and new features are added

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,12 +6,18 @@
       v-model="newValue"
       @keyup.enter="addNewTodo"
     />
-    <div v-for="(item, index) in todos" :key="item.id " class="todo-item">
-      <div class="todo">
-        <a-checkbox
-          v-model="item.completed"
-        />
-        <div class="todo-item-input">
+    <transition-group
+      name="fade"
+      enter-active-class="animated fadeInUp"
+      leave-active-class="animated fadeOutDown"
+      class="body"
+    >
+      <div v-for="(item, index) in todosFiltered" :key="item.id " class="todo-item">
+        <div class="todo">
+          <a-checkbox
+            v-model="item.completed"
+          />
+          <div class="todo-item-input">
           <span
             v-if="!item.editing"
             class="todo-item-label"
@@ -19,23 +25,23 @@
           >
             {{item.title}}
           </span>
-          <a-input
-          v-else
-          type="text"
-          v-model="item.title"
-          v-focus
-          class="item-edit-input"
-        />
+            <a-input
+              v-else
+              type="text"
+              v-model="item.title"
+              v-focus
+              class="item-edit-input"
+            />
+          </div>
+        </div>
+        <div class="actions">
+          <a-icon class="icons" type="delete" v-if="!item.editing" @click="removeTodo(index)"/>
+          <a-icon class="icons" type="edit" v-if="!item.editing" @click.once="editTodo(item)" />
+          <a-icon class="icons" type="check" v-if="item.editing" @click="doneEditing(item)"/>
+          <a-icon class="icons" type="close" v-if="item.editing" @click="cancelEditing(item)"/>
         </div>
       </div>
-      <div class="actions">
-        <a-icon class="icons" type="delete" v-if="!item.editing" @click="removeTodo(index)"/>
-        <a-icon class="icons" type="edit" v-if="!item.editing" @click.once="editTodo(item)" />
-        <a-icon class="icons" type="check" v-if="item.editing" @click="doneEditing(item)"/>
-        <a-icon class="icons" type="close" v-if="item.editing" @click="cancelEditing(item)"/>
-      </div>
-    </div>
-
+    </transition-group>
     <div class="remaining">
       <div>
         <a-checkbox :checked="!anyRemaining" @change="checkAllTodos"/>
@@ -44,6 +50,48 @@
         </label>
       </div>
       <p class="remaining-count"><span>{{ remaining }}</span>  items left</p>
+    </div>
+
+    <div class="remaining">
+      <div>
+        <a-button
+          size="small"
+          class="filter-button"
+          :class="{active: filter === 'all'}"
+          @click="filter='all'"
+        >
+          All
+        </a-button>
+        <a-button
+          size="small"
+          class="filter-button"
+          :class="{active: filter === 'active'}"
+          @click="filter='active'"
+        >
+          Active
+        </a-button>
+        <a-button
+          size="small"
+          class="filter-button"
+          :class="{active: filter === 'completed'}"
+          @click="filter='completed'"
+        >
+          Completed
+        </a-button>
+      </div>
+      <transition
+        enter-active-class="animated fadeInUp"
+        leave-active-class="animated fadeOutDown"
+      >
+        <a-button
+          size="small"
+          class="filter-button"
+          v-if="showClearCompletedButton"
+          @click="clearCompleted"
+        >
+          Clear Completed
+        </a-button>
+      </transition>
     </div>
   </div>
 </template>
@@ -56,6 +104,7 @@ export default {
       newValue: '',
       generateId: 3,
       beforeEditCache: '',
+      filter: 'all',
       todos: [
         {
           id: 1,
@@ -96,6 +145,21 @@ export default {
     },
     anyRemaining() {
       return this.remaining !== 0;
+    },
+    todosFiltered() {
+      switch (this.filter) {
+        case 'all':
+          return this.todos;
+        case 'active':
+          return this.todos.filter(el => !el.completed);
+        case 'completed':
+          return this.todos.filter(el => el.completed);
+        default:
+          return this.todos;
+      }
+    },
+    showClearCompletedButton() {
+      return this.todos.filter(el => el.completed).length > 0;
     },
   },
   directives: {
@@ -144,10 +208,14 @@ export default {
       // eslint-disable-next-line no-param-reassign,no-return-assign
       this.todos.forEach(el => el.completed = event.target.checked);
     },
+    clearCompleted() {
+      this.todos = this.todos.filter(el => !el.completed);
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
   @import "./TodoList.scss";
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css");
 </style>
